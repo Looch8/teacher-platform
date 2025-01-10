722eea9c7200eccf025134ae1551d031aae9a3db
Luke Wheldale <lukewheldale@gmail.com>
2025-01-11 10:17:44 +1030

    landing page change

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,14 +3,11 @@ import { Link } from 'react-router-dom';
 function HomePage() {
 	return (
 		<>
-			<h1>Teacher Platform</h1>
+			<h1>Assessment Mode</h1>
 			<div className="mode-container">
 				<Link to="/revision">
-					<button>Revision Mode</button>
+					<button>Let's Begin</button>
 				</Link>
-				<h2 className="assessment-mode-btn">
-					Assessment Mode (coming soon)
-				</h2>
 			</div>
 		</>
 	);
